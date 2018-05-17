fe2d003016cae0fea97fc87a36a81391fe42fe4a
Xavier <xavier@c-est-simple.com>
2018-05-17 09:04:45 +0200

    Initial commit

---
+++ XUtils.cpp
@@ -0,0 +1,39 @@
+/**
+ *  Useful stuff 
+ *  Xavier Grosjean 2017
+ *  Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License
+ */
+ 
+#include "XUtils.h"
+
+// return true if current time is after given time + delay
+bool XUtils::isElapsedDelay(unsigned long now, unsigned long* lastTime, unsigned long delay) {
+  unsigned long elapsed = now - *lastTime;
+  bool result = false;
+  // millis() overflows unsigned long after about 50 days => 0  but since unsigned,
+  // no problem !
+  if(elapsed >= delay){
+    result = true;
+  }
+  if(lastTime == 0) {
+    result = true;
+  }
+  if(result) {
+    *lastTime = now;
+  }
+  return result;
+}
+
+// Copy one string into another making sure there is no length overflow
+void XUtils::safeStringCopy(char* to, const char* from, unsigned int length) {
+  strncpy(to, from, length);
+  to[length] = 0;
+}
+
+// convert a String into a char array that will need to be freed
+// Beware, won't handle multibyte character sets 
+void XUtils::stringToCharP(String in, char** out) {
+  unsigned int size = in.length() + 1;
+  *out = (char *)malloc(size);
+  in.toCharArray(*out, size);
+}
---
+++ XUtils.h
@@ -0,0 +1,15 @@
+/**
+ *  Useful stuff 
+ *  Xavier Grosjean 2017
+ *  Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License
+ */
+#pragma once
+#include <Arduino.h>
+
+class XUtils {
+public: 
+  static bool isElapsedDelay(unsigned long now, unsigned long* lastTime, unsigned long delay);
+  static void safeStringCopy(char* to, const char* from, unsigned int length);
+  static void stringToCharP(String in, char** out);
+};
+
